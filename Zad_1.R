7550d7c96eeead06445bb8020612da27d9a3191d
Vasily-Rashkovich <47903745+Vasily-Rashkovich@users.noreply.github.com>
2019-03-30 14:55:26 +0300

    Все готово

--- Zad_1.R
+++ Zad_1.R
@@ -2,34 +2,35 @@ library(tidyverse)
 library(rnoaa)
 library(lubridate)
 
+af = c(0.00,0.00,0.00,32.11, 26.31,25.64,23.20,18.73,16.30,13.83,0.00,0.00)
+bf = c(0.00, 0.00, 0.00, 11.30, 9.26, 9.03,8.16, 6.59, 5.73, 4.87, 0.00, 0.00)
+di = c(0.00,0.00,0.00,0.33,1.00,1.00,1.00,0.32,0.00,0.00,0.00,0.00)
+Kf = 300
+Qj = 1600
+Lj = 2.2 
+Ej = 25
+
 station_data=read.csv('station_data.csv',header = TRUE,sep=",",dec=".")
 Voronezh=data.frame(id="VORONEZH",latitude=51.6701,longitude=39.2105)
 Voronezh_around=meteo_nearby_stations(lat_lon_df = Voronezh, station_data = station_data,
-                                      limit=15, var = c("PRCP","TAVG"),
+                                      limit=15, var = "all",
                                       year_min = 2007, year_max=2009)
-All_voronezh_data=list()
-for(i in 1:15){
+All_voronezh_data=tibble()
+for(i in c(1,5,8,12:15)){
   Voronezh_id=Voronezh_around[["VORONEZH"]][["id"]][i]
-  All_voronezh_data[i]=list(meteo_tidy_ghcnd(stationid = Voronezh_id,
-                                             var=c("PRCP","TAVG"),
-                                             date_min = "2007-04-01",
-                                             date_max = "2009-10-31"))}
+   st_data=meteo_tidy_ghcnd(stationid = Voronezh_id,
+                             var="all",
+                             date_min = "2007-01-01",
+                             date_max = "2009-12-31")
+    All_voronezh_data = bind_rows(All_voronezh_data, st_data %>%
+                           mutate(year = year(date), month = month(date)) %>%
+                           group_by(month, year) %>%
+                           summarise (tavg = sum(tavg[tavg>50])/10 ))}
   
-act_sum5=vector()
-d_coeff=vector()
-
-for(year in 2007:2009){
-  for(month in 4:10){
-    month_sort=vector()
-    for(station in 1:15){
-      month_sort=(filter(All_voronezh_data[[station]],
-                         date>=ymd(paste(toString(year),
-                                         toString(month),"01",sep="-"))
-                         &date<ymd(paste(toString(year),toString(month+1),"01",sep="-"))))
-      c[station]=sum(month_sort[month_sort>50],na.rm = TRUE)/10
-    }
-    act_sum5=c(act_sum5,mean(a))
-    d_coeff=c(d_coeff,length(month_sort[month_sort>70]/(length(month_sort[station]))))
-  }
-}
+Voronezh_clean=All_voronezh_data%>%
+  group_by(month) %>%
+  summarise(s = mean(tavg, na.rm = TRUE)) %>%
+  mutate (a = af, b = bf) %>%
+  mutate (fert = ((a + b * 1.0 * s) * di * Kf) / (Qj * Lj * (100-Ej)) )
+Yield = sum(Voronezh_clean$fert); Yield
   
